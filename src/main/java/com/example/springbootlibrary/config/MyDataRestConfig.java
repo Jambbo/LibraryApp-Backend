d73764b3fe8b16a34db37fd85b3262dad3598f30
Gleb <chibgleb99@gmail.com>
2024-09-05 14:30:17 +0100

    modified configuration for unsupported call methods

--- src/main/java/com/example/springbootlibrary/config/MyDataRestConfig.java
+++ src/main/java/com/example/springbootlibrary/config/MyDataRestConfig.java
@@ -5,14 +5,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+    private String allowedOrigins = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(
+            RepositoryRestConfiguration config,
+            CorsRegistry cors
+    ) {
         HttpMethod[] unsupportedMethods = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.PATCH};
 
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(Book.class, config, unsupportedMethods);
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(allowedOrigins);
+    }
+
+    private void disableHttpMethods(Class<Book> bookClass,
+                                    RepositoryRestConfiguration config,
+                                    HttpMethod[] unsupportedMethods
+    ) {
         config.getExposureConfiguration()
-                .forDomainType(Book.class)
+                .forDomainType(bookClass)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods));
     }
--- src/main/java/com/example/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/example/springbootlibrary/dao/BookRepository.java
@@ -4,6 +4,6 @@ import com.example.springbootlibrary.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
